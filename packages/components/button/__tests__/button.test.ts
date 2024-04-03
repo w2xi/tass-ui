82677cb432b1cf987656fabd4598f4fc1d44ac06
w2xi <43wangxi@gmail.com>
2024-04-03 18:34:23 +0800

    test: button

--- packages/components/button/__tests__/button.test.ts
+++ packages/components/button/__tests__/button.test.ts
@@ -20,14 +20,129 @@ describe('test Button', () => {
     });
 
     // Assert the rendered text of the component
-    expect(wrapper.text()).toContain('Hello world');
+    expect(wrapper.text()).toBe('Hello world');
   });
-  it('should have class', () => {
+  it('should have type', () => {
+    ['default', 'primary', 'success', 'info', 'danger', 'warning'].forEach(type => {
+      const wrapper = mount(button, {
+        props: {
+          type
+        }
+      });
+      expect(wrapper.classes()).toContain(`tas-button--${type}`);
+    });
+  });
+
+  it('should have size', () => {
+    ['default', 'medium', 'small', 'mini', 'tiny'].forEach(size => {
+      const wrapper = mount(button, {
+        props: {
+          size
+        }
+      });
+      expect(wrapper.classes()).toContain(`tas-button--${size}`);
+    });
+  });
+
+  it('should have plain', () => {
     const wrapper = mount(button, {
       props: {
-        type: 'primary'
+        plain: true
       }
     });
-    expect(wrapper.classes()).toContain('tas-button--primary');
+    expect(wrapper.classes()).toContain('is-plain');
+  });
+
+  it('should have round', () => {
+    const wrapper = mount(button, {
+      props: {
+        round: true
+      }
+    });
+    expect(wrapper.classes()).toContain('is-round');
+  });
+
+  it('should have circle', () => {
+    const wrapper = mount(button, {
+      props: {
+        circle: true
+      }
+    });
+    expect(wrapper.classes()).toContain('is-circle');
+  });
+
+  it('should be loading', async () => {
+    const wrapper = mount(button, {
+      props: {
+        loading: true
+      }
+    });
+    expect(wrapper.find('.tas-icon-loading').exists()).toBe(true);
+    expect(wrapper.classes()).toContain('is-loading');
+
+    await wrapper.setProps({ loading: false });
+
+    expect(wrapper.find('.tas-icon-loading').exists()).toBe(false);
+  });
+
+  it('should be disabled', async () => {
+    const wrapper = mount(button, {
+      props: {
+        disabled: true
+      }
+    });
+
+    expect(wrapper.attributes()).toHaveProperty('disabled');
+    expect(wrapper.classes()).toContain('is-disabled');
+
+    await wrapper.setProps({ disabled: false });
+
+    expect(wrapper.attributes()).not.toHaveProperty('disabled');
+    expect(wrapper.classes()).not.toContain('is-disabled');
+  });
+
+  it('should have icon', () => {
+    const wrapper = mount(button, {
+      props: {
+        icon: 'tas-icon-moon'
+      }
+    });
+    expect(wrapper.find('.tas-icon-moon').exists()).toBe(true);
+  });
+
+  it('should autoFocus', () => {
+    const wrapper = mount(button, {
+      props: {
+        autoFocus: true
+      }
+    });
+    expect(wrapper.attributes('autofocus')).toBeTruthy();
+  });
+
+  it('should have nativeType', () => {
+    ['button', 'submit', 'reset'].forEach(type => {
+      const wrapper = mount(button, {
+        props: {
+          nativeType: type
+        }
+      });
+      expect(wrapper.attributes('type')).toBe(type);
+    });
+  });
+
+  it('should click', () => {
+    const wrapper = mount(button);
+    wrapper.trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('click');
+  });
+
+  it('should not click when button is disabled', () => {
+    const wrapper = mount(button, {
+      props: {
+        disabled: true
+      }
+    });
+    wrapper.trigger('click');
+    expect(wrapper.emitted()).not.toHaveProperty('click');
   });
 });
